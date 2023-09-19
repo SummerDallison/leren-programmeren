6505382d90c916d4705be66e35cf1e92885e5ff0
SummerDallison <99068199@mydavinci.nl>
2023-09-19 13:37:14 +0200

    formateren kun je leren toegebracht

--- module1/deel1/feestlunch.py
+++ module1/deel1/feestlunch.py
@@ -14,3 +14,5 @@ kortingsbonnen_waarde = aantal_kortingsbonnen * waarde_per_kortingsbon/100
 te_betalen = totale_kosten - kortingsbonnen_waarde
 
 print(f"Te betalen bedrag in euro: {te_betalen}")
+
+print(f"De feestlunch kost je bij de bakker {te_betalen} euro voor de {aantal_croissantjes} croissantjes en de {aantal_stokbroden} stokbroden als de {aantal_kortingsbonnen} kortingsbonnen nog geldig zijn!")
--- module1/deel1/speelhal.py
+++ module1/deel1/speelhal.py
@@ -12,3 +12,5 @@ totaal_bedrag_VR = (tijd_in_VR_per_person/5) * kosten_per_minuut_per_persoon * p
 totaal_bedrag = totaal_bedrag_VR + totaal_bedrag_toegangsticket
 
 print(f"Je moet in totaal {totaal_bedrag:.2f} euro betalen.")
+
+print(f"Dit geweldige dagje-uit met {personen} mensen in de Speelhal met {tijd_in_VR_per_person} minuten VR kost je maar {totaal_bedrag:.2f} euro")
