582b7f53b7963ad15f793e9ed113e43e9cdc9dc6
SummerDallison <99068199@mydavinci.nl>
2023-09-20 13:26:08 +0200

    opdracht person afgerond

---
+++ module1/deel 2/question.py
@@ -0,0 +1,6 @@
+naam = input("Hallo, wat is je naam? ")
+leeftijd = input(f"Goededag, {naam}, hoe oud ben je? ")
+lievelings_eten = input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
+lievelings_drinken = input(f"En wat drink je het liefst bij {lievelings_eten}? ")
+
+print(f"De {leeftijd} jarige {naam} drinkt het liefst {lievelings_drinken} bij {lievelings_eten}.")
