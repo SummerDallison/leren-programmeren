0d6df3cc5c1cfd5488d992fbee4178b5c10061e9
SummerDallison <99068199@mydavinci.nl>
2023-09-20 13:25:39 +0200

    opdracht speelhal 2.0 afgerond

---
+++ module1/deel 2/speelhal2.py
@@ -0,0 +1,16 @@
+personen = int(input("Met hoeveel personen gaan er naar de speelhal? "))
+
+toegangsticket_prijs = 7.45
+
+tijd_in_VR_per_person = int(input("Hoeveel tijd willen jullie in de VIP-VR GameSeat zitten? "))
+kosten_per_minuut_per_persoon = 37
+
+totaal_bedrag_toegangsticket = toegangsticket_prijs * personen
+
+totaal_bedrag_VR = (tijd_in_VR_per_person/5) * kosten_per_minuut_per_persoon * personen/100
+
+totaal_bedrag = totaal_bedrag_VR + totaal_bedrag_toegangsticket
+
+print(f"Je moet in totaal {totaal_bedrag:.2f} euro betalen.")
+
+print(f"Dit geweldige dagje-uit met {personen} mensen in de Speelhal met {tijd_in_VR_per_person} minuten VR kost je maar {totaal_bedrag:.2f} euro")
