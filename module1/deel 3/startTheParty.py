22168b697057a8f363898087a962bc8a0f3bc2cd
SummerDallison <99068199@mydavinci.nl>
2023-09-25 13:45:39 +0200

    start the party bijgewerkt

--- module1/deel 3/startTheParty.py
+++ module1/deel 3/startTheParty.py
@@ -1,17 +1,17 @@
-gastheer_naam = input("Voer je naam in (Laat leeg als er geen gastheer is) : ").lower()
+gastheer_naam = input("Voer je naam in (Laat leeg als er geen gastheer is): ").lower()
 
 gasten = True
 drank = True
 chips = True
 
 mijn_naam = gastheer_naam == "summer" or gastheer_naam == "summer dallison"
-lsb_docent_naam = gastheer_naam != "wilfred" or gastheer_naam != "wilfred bouwman"
+lsb_docent_naam = gastheer_naam == "wilfred" or gastheer_naam == "wilfred bouwman"
 
 if mijn_naam:
     print("Er is hoe dan ook een feest.")
 elif lsb_docent_naam:
     print("Er is hoe dan ook geen feest.")
-elif(gasten and (chips and drank)) or (gastheer_naam and drank or (gasten or chips)):
+elif (gasten and (chips and drank)) or (gastheer_naam and (drank and gasten or chips)):
     print('Start the Party')
 else:
     print('No Party')
