357e2add110e2298d13a45926546442c40ceea69
SummerDallison <99068199@mydavinci.nl>
2023-09-27 14:06:17 +0200

    studieadvies bijgewerkt

---
+++ module1/deel 3/studieadvies.py
@@ -0,0 +1,23 @@
+print("***********************STUDIEADVIES***********************")
+print("Ik ga jou helpen met jouw opleiding.")
+print("Jij krijgt een aantal stellingen te zien.")
+print("Voor elke stelling moet je zeggen hoeveel dat bij jou voorkomt.")
+print("Je kunt steeds antwoorden:")
+print("0 is 'altijd'")
+print("1 is 'vaak'")
+print("2 is 'regelmatig'") 
+print("3 is 'soms'") 
+print("4 is 'nooit'.")
+print("Het is belangrijk om eerlijk te zijn.")
+print("Op basis van jouw antwoorden krijg je advies.")
+
+input("Stelling 1: 'Ik voel stress of blokkades bij het maken van programmeeropdrachten.'")
+input("Stelling 2: 'Ik vergelijk mezelf met anderen die beter lijken te zijn.'")
+input("Stelling 3: 'Ik denk dat ik geen talent heb voor de opleiding.'")
+input("Stelling 4: 'Ik stel programmeeropdrachten en -uitdagingen uit.'")
+input("Stelling 5: 'Ik vermijd assessments (CJV) en feedback van kritische docenten.'")
+input("Stelling 6: 'Ik voel geen interesse in nieuwe programmeertechnieken.'")
+input("Stelling 7: 'Ik kopieer code van anderen en lever dat in alsof het helemaal van mij is.'")
+
+antwoorden = []
+
