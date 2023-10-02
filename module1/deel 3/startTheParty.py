05f2ce2f7876a7e3ee4fd42f3faba4d1cbf2a388
SummerDallison <99068199@mydavinci.nl>
2023-10-02 11:40:08 +0200

    afgerond

--- module1/deel 3/startTheParty.py
+++ module1/deel 3/startTheParty.py
@@ -1,9 +1,22 @@
-gastheer = False
-gasten = True
+gastheer_naam = input("Voer je naam in (Laat leeg als er geen gastheer is): ").lower()
+
+gasten = int(input("Voer aantal gasten in : "))
+
 drank = True
 chips = True
 
-if (gasten and (chips and drank)) or (gastheer and drank or (gasten or chips)):
+mijn_naam = gastheer_naam == "summer" or gastheer_naam == "summer dallison"
+lsb_docent_naam = gastheer_naam == "wilfred" or gastheer_naam == "wilfred bouwman"
+
+if mijn_naam:
+    print("Er is hoe dan ook een feest.")
+elif lsb_docent_naam:
+    print("Er is hoe dan ook geen feest.")
+elif gasten < 4:
+    print("Het feest kan pas beginnen wanneer er minimaal 4 gasten zijn.")
+elif gasten > 20:
+    print("Een feest kan niet starten wanneer er meer dan 20 gasten aanwezig zijn.")
+elif (gasten and chips and drank) or (gastheer_naam and drank):
     print('Start the Party')
 else:
     print('No Party')
