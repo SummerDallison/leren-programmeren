89968baef1dac465ad20f66f4c0af20888416921
SummerDallison <99068199@mydavinci.nl>
2023-10-02 11:13:53 +0200

    opdracht studieadvies afgerond

--- module1/deel 3/studieadviestext.py
+++ module1/deel 3/studieadviestext.py
@@ -7,41 +7,36 @@ print("Het is belangrijk om eerlijk te zijn. Op basis van jouw antwoorden krijg 
 
 AANTAL_OPTIES = "Kies 0: altijd; 1: vaak; 2: regelmatig; 3: soms; 4: nooit"
 
-aantal_weken_opleiding = int(input('Hoeveel weken ben je al bezig met de opleiding?'))
+aantal_weken_opleiding = int(input('Hoeveel weken ben je al bezig met de opleiding? '))
 
 antwoorden = []
 
-antwoorden.append(int(input('Ik voel stress of blokkades bij het maken van programmeeropdrachten.')))
-antwoorden.append(int(input('Ik stel programmeeropdrachten en -uitdagingen uit.')))
-antwoorden.append(int(input('Ik denk dat ik geen talent heb voor de opleiding.')))
+antwoorden.append(int(input('Ik voel stress of blokkades bij het maken van programmeeropdrachten. ')))
+antwoorden.append(int(input('Ik stel programmeeropdrachten en -uitdagingen uit. ')))
+antwoorden.append(int(input('Ik denk dat ik geen talent heb voor de opleiding. ')))
 antwoorden.append(int(input('Ik vermijd assessments (CJV) en feedback van kritische docenten. ')))
-antwoorden.append(int(input('Ik vergelijk mezelf met anderen die beter lijken te zijn.')))
+antwoorden.append(int(input('Ik vergelijk mezelf met anderen die beter lijken te zijn. ')))
 
 if aantal_weken_opleiding >= 10:
-    antwoorden.append(int(input('Ik voel geen interesse in nieuwe programmeertechnieken.')))
-    antwoorden.append(int(input('Ik kopieer code van anderen en lever dat in alsof het helemaal van mij is.')))
+    antwoorden.append(int(input('Ik voel geen interesse in nieuwe programmeertechnieken. ')))
+    antwoorden.append(int(input('Ik kopieer code van anderen en lever dat in alsof het helemaal van mij is. ')))
 
 gemiddelde_score = sum(antwoorden)/len(antwoorden)
 
-if gemiddelde_score <= 2:
-    advies = COMPETENTIE_ADVIES_ZORGELIJK
-elif gemiddelde_score <= 3:
-    advies = "Advies is 'Twijfelachtig'."
-else:
-    advies = "Advies is 'geruststellend'."
+COMPETENTIE_ADVIES_TITEL = ("*********************** STUDIEADVIES ***********************")
 
-COMPETENTIE_ADVIES_TITEL = '''
-*********************** STUDIEADVIES ***********************'''
-COMPETENTIE_ADVIES_ZORGELIJK = '''
-Het lijkt erop dat je nog weinig zelfvertrouwen, voldoening en plezier ervaart 
-in het leren programmeren. Er zijn altijd mogelijkheden om je te helpen de draad 
-op te pakken met de opleiding. Praat met jouw SLB-er over extra begeleiding en oefeningen.
-'''
-COMPETENTIE_ADVIES_TWIJFELACHTIG = '''
-Het lijkt erop dat je af en toe weinig zelfvertrouwen, voldoening of plezier ervaart
-in het leren programmeren. Houd je zelfvertrouwen en motivatie in de gaten!
-'''
-COMPETENTIE_ADVIES_GERUSTSTELLEND = '''
-Het lijkt erop dat je voldoende zelfvertrouwen, voldoening en plezier ervaart in
-het leren programmeren. Mocht het een keer minder gaan, maak je geen zorgen. Have fun!
-'''
+COMPETENTIE_ADVIES_ZORGELIJK = ("Het lijkt erop dat je nog weinig zelfvertrouwen, voldoening en plezier ervaart in het leren programmeren. Er zijn altijd mogelijkheden om je te helpen de draad op te pakken met de opleiding. Praat met jouw SLB-er over extra begeleiding en oefeningen.")
+
+COMPETENTIE_ADVIES_TWIJFELACHTIG = ("Het lijkt erop dat je af en toe weinig zelfvertrouwen, voldoening of plezier ervaart in het leren programmeren. Houd je zelfvertrouwen en motivatie in de gaten!")
+
+COMPETENTIE_ADVIES_GERUSTSTELLEND = ("Het lijkt erop dat je voldoende zelfvertrouwen, voldoening en plezier ervaart in het leren programmeren. Mocht het een keer minder gaan, maak je geen zorgen. Have fun!")
+
+print(COMPETENTIE_ADVIES_TITEL)
+
+if gemiddelde_score <= 2:
+    print(COMPETENTIE_ADVIES_ZORGELIJK)
+elif gemiddelde_score <= 3:
+    print(COMPETENTIE_ADVIES_TWIJFELACHTIG)
+else:
+    print(COMPETENTIE_ADVIES_GERUSTSTELLEND)
+
