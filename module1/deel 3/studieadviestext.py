b559a439c0df831f9427a9bfac44c121aafd7f4b
SummerDallison <99068199@mydavinci.nl>
2023-10-02 14:15:56 +0200

    bijgewerkt

--- module1/deel 3/studieadviestext.py
+++ module1/deel 3/studieadviestext.py
@@ -1,4 +1,3 @@
-
 print("*********************** STUDIEADVIES ***********************")
 print("Ik ga jou helpen met jouw opleiding. Jij krijgt een aantal stellingen te zien.")
 print("Voor elke stelling moet je zeggen hoeveel dat bij jou voorkomt. Je kunt steeds")
@@ -11,7 +10,7 @@ aantal_weken_opleiding = int(input('Hoeveel weken ben je al bezig met de opleidi
 
 antwoorden = []
 
-antwoorden.append(int(input(f'Ik voel stress of blokkades bij het maken van programmeeropdrachten.{AANTAL_OPTIES} ')))
+antwoorden.append(int(input('Ik voel stress of blokkades bij het maken van programmeeropdrachten. ')))
 antwoorden.append(int(input('Ik stel programmeeropdrachten en -uitdagingen uit. ')))
 antwoorden.append(int(input('Ik denk dat ik geen talent heb voor de opleiding. ')))
 antwoorden.append(int(input('Ik vermijd assessments (CJV) en feedback van kritische docenten. ')))
