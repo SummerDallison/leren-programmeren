8711447fb803834d07c127c169042a0e16b7a1a0
SummerDallison <99068199@mydavinci.nl>
2023-10-11 11:14:04 +0200

    bijgewerkt

--- module1/deel 5/grootste_function.py
+++ module1/deel 5/grootste_function.py
@@ -1,7 +1,10 @@
 def determine_biggest(nr1: int, nr2: int) -> str:
     if nr1 < nr2:
-        return (f"Maximum: {nr2} and minimum: {nr1}") 
+        print(f"Maximum: {nr2} and minimum: {nr1}")
+        return str(f"Maximum: {nr2} and minimum: {nr1}") 
     elif nr1 > nr2:
-        return (f"Maximum: {nr1} and minimum: {nr2}")
+        print(f"Maximum: {nr1} and minimum: {nr2}")
+        return str(f"Maximum: {nr1} and minimum: {nr2}") 
     else:
-        return "Beide getallen zijn even groot"
+        print("Beide getallen zijn even groot")
+        return str("Beide getallen zijn even groot")
