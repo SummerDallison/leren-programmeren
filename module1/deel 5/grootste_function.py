4c5288c0dd6931490a6cd9b4d073838693812b57
SummerDallison <99068199@mydavinci.nl>
2023-10-11 11:50:15 +0200

    opdracht grootste function afgerond

--- module1/deel 5/grootste_function.py
+++ module1/deel 5/grootste_function.py
@@ -1,10 +1,31 @@
+from test_lib import test, report
+
 def determine_biggest(nr1: int, nr2: int) -> str:
     if nr1 < nr2:
-        print(f"Maximum: {nr2} and minimum: {nr1}")
-        return str(f"Maximum: {nr2} and minimum: {nr1}") 
+        antwoord = (f"Maximum: {nr2} and minimum: {nr1}") 
     elif nr1 > nr2:
-        print(f"Maximum: {nr1} and minimum: {nr2}")
-        return str(f"Maximum: {nr1} and minimum: {nr2}") 
+        antwoord = (f"Maximum: {nr1} and minimum: {nr2}")
     else:
-        print("Beide getallen zijn even groot")
-        return str("Beide getallen zijn even groot")
+        antwoord = ("Beide getallen zijn even groot")
+
+    return antwoord
+
+nr1 = 10
+nr2 = 20
+verwacht = (f"Maximum: {nr2} and minimum: {nr1}") 
+resultaat = determine_biggest(nr1, nr2)
+test("Test 1", verwacht, resultaat)
+
+nr1 = 20
+nr2 = 10
+verwacht = (f"Maximum: {nr1} and minimum: {nr2}")
+resultaat = determine_biggest(nr1, nr2)
+test("Test 2", verwacht, resultaat)
+
+nr1 = 10
+nr2 = 10
+verwacht = "Beide getallen zijn even groot"
+resultaat = determine_biggest(nr1, nr2)
+test("Test 3", verwacht, resultaat)
+
+report()
