ef0c39f2d2a989e2b1b6a4cf8aa2a13d2e1120da
SummerDallison <99068199@mydavinci.nl>
2023-10-23 13:20:13 +0200

    added opzoeken

---
+++ module1/deel 5/real_complex_functions1.py
@@ -0,0 +1,14 @@
+def get_value(data: str, separator: str, position: int) -> str:
+   splitted_strings = data.split(separator)
+   
+   if 0 <= position< len(splitted_strings):
+      return splitted_strings[position]
+   else:
+      return None
+
+toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
+separator = ','
+position = 8
+
+result = get_value(toets_data, separator, position)
+print(result)
