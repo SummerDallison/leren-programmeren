fa418ed997f40dd9fcdf764afbb115c95b10a471
SummerDallison <99068199@mydavinci.nl>
2023-10-31 09:14:05 +0100

    bijgewerkt

--- module1/proef/Amnesia in the Realm of Enchantment.py
+++ module1/proef/Amnesia in the Realm of Enchantment.py
@@ -2,7 +2,7 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(">          Amnesia in the Realm of Enchantment          >")
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-print("")
+print("Ik opende mijn ogen en ")
 
 def vraag_naam():
     while True:
@@ -11,30 +11,14 @@ def vraag_naam():
             print("Ongeldige invoer. Voer alleen letters in uw naam.")
             naam = input("Voer uw naam in: ")
 
+        bevestiging_naam = input(f"Uw naam is {naam}, klopt dit? (ja/nee): ").lower()
+        while bevestiging_naam not in ["ja", "nee"]:
+            print("Antwoord alstublieft met 'ja' of 'nee'.")
             bevestiging_naam = input(f"Uw naam is {naam}, klopt dit? (ja/nee): ").lower()
-            while bevestiging_naam not in ["ja", "nee"]:
-                print("Antwoord alstublieft met 'ja' of 'nee'.")
-                bevestiging_naam = input(f"Uw naam is {naam}, klopt dit? (ja/nee): ").lower()
 
-            if bevestiging_naam == "ja":
-                break
+        if bevestiging_naam == "ja":
+            return naam
 
-vraag_naam()
+naam = vraag_naam()
 
-leeftijd = input("Voer uw leeftijd in: ")
-
-while True:
-    try:
-        leeftijd = int(leeftijd)
-        if leeftijd >= 0:
-            leeftijd_bevestiging = input(f"Uw leeftijd is {leeftijd}, klopt dit? (ja/nee): ")
-            if leeftijd_bevestiging.lower() == "ja":
-                break
-            else:
-                leeftijd = input("Voer uw leeftijd in: ")
-        else:
-            print("Ongeldige invoer. Voer een positieve numerieke leeftijd in.")
-            leeftijd = input("Voer uw leeftijd in: ")        
-    except ValueError:
-        print("Ongeldige invoer. Voer een numerieke leeftijd in.")
-        leeftijd = input("Voer uw leeftijd in: ")
+print(f"'{naam}', mompelde ik. ")
