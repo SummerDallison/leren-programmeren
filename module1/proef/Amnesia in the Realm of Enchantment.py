6bd258ea28e33fd6bc626fe7107335f442e5f40e
SummerDallison <99068199@mydavinci.nl>
2023-11-01 13:42:27 +0100

    bijgewerkt

--- module1/proef/Amnesia in the Realm of Enchantment.py
+++ module1/proef/Amnesia in the Realm of Enchantment.py
@@ -4,6 +4,7 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 inventaris = []
 companions = []
+profiel_inhoud = []
 
 print("")
 
@@ -24,7 +25,23 @@ def vraag_naam():
 
 naam = vraag_naam()
 
-print(f"'{naam},' mompel ik, alsof ik het voor het eerst hoor, maar het klinkt ook zo vertrouwd. ")
+print(f"'{naam},' mompel ik, alsof ik het voor het eerst hoor, ")
+print("maar het klinkt ook zo vertrouwd. ")
 
-print("[Nieuwe informatie is onthuld: ]")
-print(f"[naam: {naam}]")
+print("[Nieuwe informatie is toegevoegd aan uw profiel!]")
+
+nieuwe_informatie = (f"[Naam: {naam}]")
+
+while True:
+    profiel = input("Open profiel? (ja/nee) ")
+    if profiel.lower() == "ja":
+        profiel_inhoud.append(f"{nieuwe_informatie}")
+        break
+    elif profiel.lower() == "nee":
+        print("[Je hebt ervoor gekozen je profiel niet te openen. Het verhaal gaat verder.]")
+        break
+    else:
+        print("Antwoord alstublieft met 'ja' of 'nee'.")
+
+for extra_info in profiel_inhoud:
+    print(extra_info)
