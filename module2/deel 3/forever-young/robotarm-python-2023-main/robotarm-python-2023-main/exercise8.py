ba09c74e6f6933287b5e1f0703e7be41b9a4a3b4
SummerDallison <99068199@mydavinci.nl>
2024-01-21 19:15:17 +0100

    afgerond

---
+++ module2/deel 3/forever-young/robotarm-python-2023-main/robotarm-python-2023-main/exercise8.py
@@ -0,0 +1,21 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 8')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+
+for stack in range(7):
+    robotArm.grab()
+    
+    for i in range(8):
+        robotArm.moveRight()
+        
+    robotArm.drop()
+
+    if stack < 6:
+        for i in range(8):
+            robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
