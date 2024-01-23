07099b2f9f286f4ba081fdd6eaeef7be0aaf18a8
SummerDallison <99068199@mydavinci.nl>
2024-01-23 13:40:10 +0100

    bijgewerkt

--- module2/deel 4/clubbing.py
+++ module2/deel 4/clubbing.py
@@ -5,4 +5,17 @@ PRIJS_CHAMPAGNE = 12.30
 DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
 
-#bouw hieronder de floowchart na
+#bouw hieronder de flowchart na
+
+while True:
+    try:
+        leeftijd = int(input("Wat is uw leeftijd? "))
+        if leeftijd < 18:
+            print("Sorry, u mag niet naar binnen.")
+            print(f"Probeer het over {18-leeftijd} jaar nog eens.")
+        elif leeftijd >= 18:
+                input("Wat is uw naam? ")
+        break
+
+    except ValueError:
+        print("Voer alstublieft uw leeftijd in.")
