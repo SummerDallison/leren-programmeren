730f0067bb847a12b2d8c8bfa351c602a839a001
SummerDallison <99068199@mydavinci.nl>
2024-01-23 11:35:13 +0100

    afgerond

--- module2/deel 3/forever-young/robotarm-python-2023-main/robotarm-python-2023-main/exercise12.py
+++ module2/deel 3/forever-young/robotarm-python-2023-main/robotarm-python-2023-main/exercise12.py
@@ -3,7 +3,24 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
+for i in range(8):
+    robotArm.moveRight()
 
+for i in range(9):
+    robotArm.grab()
+    color = robotArm.scan()
+    print(robotArm.scan())
+    if color == "red":
+        for x in range(1 + i):
+            robotArm.moveRight()
+        robotArm.drop()
+        for x in range(2 + i):
+            if i < 8:
+                robotArm.moveLeft()
+    else:
+        robotArm.drop()
+        if i < 8:
+            robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
--- module2/deel 3/forever-young/robotarm-python-2023-main/robotarm-python-2023-main/exercise13.py
+++ module2/deel 3/forever-young/robotarm-python-2023-main/robotarm-python-2023-main/exercise13.py
@@ -1,9 +1,22 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 13')
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 
+while True:
+    robotArm.grab()
+
+    color = robotArm.scan()
+    print(robotArm.scan())
+
+    if color != "":
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveLeft()
+    else:
+        break
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
