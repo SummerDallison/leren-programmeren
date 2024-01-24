d834566820d19e4a226a7236ddc195f5900073bd
SummerDallison <99068199@mydavinci.nl>
2024-01-24 10:58:28 +0100

    bijgewerkt

--- module2/deel 4/clubbing.py
+++ module2/deel 4/clubbing.py
@@ -10,11 +10,32 @@ VIP_LIST = ('jeroen', 'jouke', 'rudi')
 while True:
     try:
         leeftijd = int(input("Wat is uw leeftijd? "))
+        
         if leeftijd < 18:
             print("Sorry, u mag niet naar binnen.")
-            print(f"Probeer het over {18-leeftijd} jaar nog eens.")
+            print(f"Probeer het over {18 - leeftijd} jaar nog eens.")
+
         elif leeftijd >= 18:
-                input("Wat is uw naam? ")
+                
+            while True:
+                naam = input("Wat is uw naam? ")
+
+                if naam.isalpha():
+
+                    if naam.lower() in VIP_LIST:
+
+                        print("Welkom op de VIP-lijst!")
+
+                    else:
+                        
+                        print("Helaas, je staat niet op de VIP-lijst.")
+
+                else:
+                    print("Voer alstublieft uw naam in.")
+                    continue
+
+                break
+
         break
 
     except ValueError:
