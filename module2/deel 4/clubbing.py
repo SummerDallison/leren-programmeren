365f4aa8c570b4859005d76dd5757b0964731f39
SummerDallison <99068199@mydavinci.nl>
2024-02-06 10:23:51 +0100

    afgerond

--- module2/deel 4/clubbing.py
+++ module2/deel 4/clubbing.py
@@ -22,18 +22,67 @@ while True:
 
                 if naam.isalpha():
 
+                    bandje_stempel = []
+
                     if naam.lower() in VIP_LIST:
 
-                        print("Welkom op de VIP-lijst!")
+                        if leeftijd >= 21:
+                            bandje_stempel.append("blauw bandje")
+                            print(f"U krijgt van mij een {bandje_stempel[0]}.")
+
+                        else:
+                            bandje_stempel.append("rood bandje")
+                            print(f"U krijgt van mij een {bandje_stempel[0]}.")
+                    
+                    else:
+
+                        if leeftijd >= 21:
+                            bandje_stempel.append("stempel")
+                            print(f"U krijgt van mij een {bandje_stempel[0]}.")
+                    
+                    DRANKJES = input(f"Wat wilt u drinken? ")
+
+                    if DRANKJES.lower() == "cola":
+                        if "blauw bandje" in bandje_stempel or "rood bandje" in bandje_stempel:
+                            print("Alstublieft, complimenten van het huis.")
+
+                        else:
+                            print(f"Alstublieft uw {DRANKJES}, dat is dan €{PRIJS_COLA:.2f}.")
+                    
+                    elif DRANKJES.lower() == "bier":
+                        
+                        if "blauw bandje" in bandje_stempel or "rood bandje" in bandje_stempel or "stempel" in bandje_stempel:
+                            
+                            if "blauw bandje" in bandje_stempel or "rood bandje" in bandje_stempel:
+                                print("Alstublieft, complimenten van het huis.")
+                            
+                            else:
+                                print(f"Alstublieft uw {DRANKJES}, dat is dan €{PRIJS_BIER:.2f}.")
+                        
+                        else:
+                            print("Sorry, u mag geen alcohol bestellen onder de 21.")
+                            print(f"Probeer het over {21 - leeftijd} jaar nog eens.")
+
+                    elif DRANKJES.lower() == "champagne":
+
+                        if "blauw bandje" in bandje_stempel or "rood bandje" in bandje_stempel:
+
+                            if "blauw bandje" in bandje_stempel:
+                                print(f"Alstublieft uw {DRANKJES}, dat is dan €{PRIJS_CHAMPAGNE:.2f}.")
+
+                            else:
+                                print("Sorry, u mag geen alcohol bestellen onder de 21.")
+                                print(f"Probeer het over {21 - leeftijd} jaar nog eens.")
+                        
+                        else:
+                            print("Sorry, alleen vips mogen champagne bestellen.")
 
                     else:
-                        
-                        print("Helaas, je staat niet op de VIP-lijst.")
+                        print("Sorry, geen idee wat u bedoelt, hier een glaasje water.")
 
                 else:
                     print("Voer alstublieft uw naam in.")
                     continue
-
                 break
 
         break
