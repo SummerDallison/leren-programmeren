17d665236e5d0a4e4974e5431fa5989a02da87e5
SummerDallison <99068199@mydavinci.nl>
2024-02-27 12:29:53 +0100

    afgerond

---
+++ module2/proef/proef_pseudocode.py
@@ -0,0 +1,29 @@
+#Herhaal:
+    #Er wordt gevraagd om de naam van een deelnemer in te voeren
+    #Als de naam niet in deelnemers zit:
+        #Voeg naam toe aan de lootjes
+
+    #Als de naam wel toegvoegd is:
+        #Toon dat de naam al toegevoegd is aan de lootjes
+        #De naam wordt niet toegevoegd aan de lootjes
+
+        #Check aantal deelnemers:
+            #Als aantal deelnemers is minder dan 3:
+                #Er wordt gevraagd om de naam van een deelnemer in te voeren
+
+            #herhaal:
+                #Als aantal deelnemers is gelijk aan 3 of meer dan 3:
+                    #Er wordt gevraagd of je meer deelnemers wilt toevoegen:
+
+                    #Als je 'ja' invoert:
+                        #Er wordt gevraagd om de naam van een deelnemer in te voeren
+
+                    #Als je 'nee' invoert:
+                        #Krijgt iedereen een lootje
+
+                        #Herhaal tot dat niemand hun eigen lootje heeft getrokken:
+                            #Checken of iemand hun eigen lootje heeft getrokken:
+                                #Als iemand hun eigen lootje heeft getrokken:
+                                    #Lootjes worden opnieuw verdeeld
+
+#Alle namen met bijhorende lootjes worden uitgeprint   
