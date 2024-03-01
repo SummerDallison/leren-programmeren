07dcc25331f9c763f2a47d46b3e8992fee2058fa
SummerDallison <99068199@mydavinci.nl>
2024-03-01 11:51:34 +0100

    opdracht rename me afgerond

--- module 3/deel 1/rename_me.py
+++ module 3/deel 1/rename_me.py
@@ -1,28 +1,33 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool:
-    return stellar_broccoli % 2 == 0
+def even_getal(getal:int) -> bool:
+    return getal % 2 == 0 
+#Deze functie checkt of het getal even is (even: True, oneven: False).
 
-def chaos_papegaai(fantasie_platypus:str) -> str:
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
-    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
-    return tijdmachine_pannenkoekenmix
+def omgekeerde_woorden(zin:str) -> str:
+    woorden = zin.split()
+    doldwaze_broccoli = woorden[::-1]
+    omgekeerde_zin = ' '.join(doldwaze_broccoli)
+    return omgekeerde_zin 
+#Deze functie keert de volgorde van woorden in de gegeven zin om.
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
+def unieke_karakters(tekst:str) -> int:
+    unieke_karakters_set = set(tekst)
+    aantal_unieke_characters = len(unieke_karakters_set)
+    return aantal_unieke_characters
+#Deze functie telt alle letters op, maar het zijn alleen de letters die nog niet opgeteld zijn (geen dubbele letters).
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float:
-    wobbelwobbel = planetair_taartje.split()
+def gemiddelde_woordenlengte(zin:str) -> float:
+    woorden = zin.split()
     
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+    totaal_karakters = 0
+    for woord in woorden:
+        totaal_karakters += len(woord)
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    gemiddelde = totaal_karakters / len(woorden)
+    return gemiddelde
+#Deze functie berekent het gemiddelde aantal karakters per woord in de zin.
 
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+def print_vermenigvuldigingstabel(getal:int, aantal_keer:int=10) -> None:
+    for vermenigvuldiger in range(1, aantal_keer+1):
+        resultaat = vermenigvuldiger * getal
+        print(f'{vermenigvuldiger} x {getal} = {resultaat}')
+#Deze functie print de vermenigvuldigingstabel tot 10 en met het gegeven getal.
