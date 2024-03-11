75bf75838e88a17d4cf981332baadd2448242b46
SummerDallison <99068199@mydavinci.nl>
2024-03-11 16:06:41 +0100

    opdracht naam en leeftijd afgerond

---
+++ module 3/deel 1/naam_leeftijd.py
@@ -0,0 +1,24 @@
+def vraag_naam_en_leeftijd():
+    while True:
+        naam = input("Voer uw naam in: ")
+
+        if naam.isalpha():
+            break
+
+        else:
+            print("Ongeldige invoer. Voer alstublieft uw naam in.")
+
+    while True:
+        leeftijd = input("Voer uw leeftijd in: ")
+
+        if leeftijd.isdigit():
+            break
+
+        else:
+            print("Ongeldige invoer. Voer alstublieft uw leeftijd in.")
+
+    return {'name' : naam, 'age' : leeftijd}
+
+gegevens = vraag_naam_en_leeftijd()
+
+print(f"{gegevens['name']} is {gegevens['age']} jaar.")
