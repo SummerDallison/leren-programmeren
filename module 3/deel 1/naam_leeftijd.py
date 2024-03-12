6def868afef73ff083df4f3c48d99173a2e8174c
SummerDallison <99068199@mydavinci.nl>
2024-03-12 10:16:31 +0100

    opdracht bijgewerkt

--- module 3/deel 1/naam_leeftijd.py
+++ module 3/deel 1/naam_leeftijd.py
@@ -1,4 +1,4 @@
-def vraag_naam_en_leeftijd():
+def vragen_gegevens():
     while True:
         naam = input("Voer uw naam in: ")
         if naam.isalpha():
@@ -13,8 +13,32 @@ def vraag_naam_en_leeftijd():
         else:
             print("Ongeldige invoer. Voer alstublieft uw leeftijd in.")
 
-    return {'name' : naam, 'age' : leeftijd}
+    while True:
+        woonplaats = input("Voer uw woonplaats in: ")
+        if woonplaats.isalpha():
+            break
+        else:
+            print("Ongeldige invoer. Voer alstublieft uw woonplaats in.")
 
-gegevens = vraag_naam_en_leeftijd()
+    return {'name' : naam, 'age' : leeftijd, 'city' : woonplaats}
 
-print(f"{gegevens['name']} is {gegevens['age']} jaar.")
+def verzamel_gegevens():
+    gegevens_list = []
+    gegevens = vragen_gegevens()
+    gegevens_list.append(gegevens)
+    while True:
+        invoer = input("Toets enter om door te gaan of stop om te printen: ").lower()
+        if invoer == "stop":
+            break
+        elif invoer == "":
+            gegevens = vragen_gegevens()
+            gegevens_list.append(gegevens)
+        else:
+            print("Ongeldige invoer. Toets alstublieft op enter of voer 'stop' in.")
+
+    return gegevens_list
+
+gegevens = verzamel_gegevens()
+
+for data in gegevens:
+    print(f"{data['name']}, die in {data['city']} woont, is {data['age']} jaar.")
