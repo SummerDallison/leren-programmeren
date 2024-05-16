9a47260cadee336ad5f60373d65d0bf4cfd53984
SummerDallison <99068199@mydavinci.nl>
2024-05-16 11:17:43 +0200

    bijgewerkt

---
+++ module 3/deel 3/reeks.py
@@ -0,0 +1,22 @@
+#Functie om de volgende reeks te maken
+def generate_next_sequence(sequence):
+    next_sequence = ""
+    index = 0
+    while index < len(sequence):
+        count = 1
+        while index + 1 < len(sequence) and sequence[index] == sequence[index + 1]:
+            index += 1
+            count += 1
+        next_sequence += str(count) + sequence[index]
+        index += 1
+    return next_sequence
+
+#Deze functie blijft de reeks printen totdat er twee 3'en achter elkaar staan
+def find_sequence():
+    current_sequence = "1" #Start met de eerste regel
+    while "33" not in current_sequence:
+        print(current_sequence)
+        current_sequence = generate_next_sequence(current_sequence)
+    print(current_sequence)
+
+find_sequence()
