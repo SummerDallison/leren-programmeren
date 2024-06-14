5dd852dbbfee06636d28cdbf32e0cc8316af8884
SummerDallison <99068199@mydavinci.nl>
2024-06-14 11:59:05 +0200

    bijgewerkt

---
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -0,0 +1,12 @@
+TEST_ANSWERS = ['a','b','c']
+AVATAR_ANSWERS = ['Batman','Spider-Man','Steve','Joe','Thanos','Graig']
+CORRECT_AVATAR_ANSWER = ['Batman']
+
+YES_OPTIES = ['y','j','yes','ja']
+NO_OPTIES = ['n','no','nee']
+
+INTRODUCTION_TEXT = "Dag speler, dit is een adventure game waar het draait om keuzes."
+EXAMPLE_TEXT = "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen."
+QUESTION_TEXT = "A, B of C?"
+SELECT_AVATAR_TEXT = "Selecteer nu één van de volgende karakters: "
+AVATAR_CHOICES_TEXT = "Spider-Man, Joe, Thanos, Graig"
