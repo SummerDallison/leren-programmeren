fc6a5946241169abccae213627012bae0ddfa29c
SummerDallison <99068199@mydavinci.nl>
2024-06-17 09:58:50 +0200

    bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -1,3 +1,5 @@
+from termcolor import colored
+
 TEST_ANSWERS = ['a','b','c']
 AVATAR_ANSWERS = ['Batman','Spider-Man','Steve','Joe','Thanos','Graig']
 CORRECT_AVATAR_ANSWER = ['Batman']
@@ -5,8 +7,8 @@ CORRECT_AVATAR_ANSWER = ['Batman']
 YES_OPTIES = ['y','j','yes','ja']
 NO_OPTIES = ['n','no','nee']
 
-INTRODUCTION_TEXT = "Dag speler, dit is een adventure game waar het draait om keuzes."
-EXAMPLE_TEXT = "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen."
-QUESTION_TEXT = "A, B of C?"
-SELECT_AVATAR_TEXT = "Selecteer nu één van de volgende karakters: "
-AVATAR_CHOICES_TEXT = "Spider-Man, Joe, Thanos, Graig"
+INTRO_TEXTS = ["Dag speler, dit is een adventure game waar het draait om keuzes.", 'blue',
+               "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.", 'blue',
+               "A, B of C?", 'blue']
+
+AVATAR_SELECTION_TEXT = "Selecteer nu één van de volgende karakters: \nSpider-Man, Joe, Thanos, Graig"
