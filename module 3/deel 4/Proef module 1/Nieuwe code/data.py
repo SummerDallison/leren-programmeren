7347ff2aa89e4470f99a27a05e5e18e5c42a9733
SummerDallison <99068199@mydavinci.nl>
2024-06-19 14:11:59 +0200

    bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -7,8 +7,135 @@ CORRECT_AVATAR_ANSWER = 'Batman'
 YES_OPTIONS = ['y','j','yes','ja']
 NO_OPTIONS = ['n','no','nee']
 
-INTRO_TEXTS = (
-    "Dag speler, dit is een adventure game waar het draait om keuzes.",
-    "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen."
-)
-QUESTION_TEXT = "A, B of C?"
+'Dag speler, dit is een adventure game waar het draait om keuzes.'
+'Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.'
+'A, B of C?'
+'Top! het gegeven antwoord is {test}.'
+'Helaas, dit was geen keuze. Probeer het opnieuw...'
+'A, B of C?'
+
+'Selecteer nu één van de volgende karakters.'
+'Spider-Man, Joe, Thanos, Graig'
+'Je wordt om 8 uur in de ochtend wakker in je bed op halloween..'
+'Welke outfit doe je vandaag aan?'
+'Casual, Spider-Suit'
+
+'Je hebt de outfit "{outfit}" gekozen!'
+'Je gaat naar school maar je bent 5 minuten te laat.'
+'Je gaat naar je eerste les, wiskunde...'
+'Eenmaal binnen gekomen zie je dat de docent al boos kijkt.'
+'Je bent te laat, MAAR als je deze vraag goed beantwoord dan mag je blijven.'
+'Wat is de wortel van "4 x (7:(15-10))"?'
+'Top gedaan 5.6 is helemaal goed!'
+'Top of the class ending.'
+
+'Dat is helemaal fout, ga jij maar melden bij de directie'
+'Je bent op de gang onderweg naar de directie'
+'Alleen omdat ik maar 5 minuutjes te laat ben'
+'Ga je meldeen bij directie?'
+
+'Waarom ben je eruit gestuurd?'
+'Omdat ik 5 minuutjes te laat was...'
+'Je bent gecatched door directie, laat je nooit vangen!'
+
+'WAT JE GAAT NIET, IK WIST NIET DAT JE DIT IN JE HAD'
+'RENNEN!!!!!'
+'Je rent heel hard door de gangen van de school!'
+'Achter je merk je dat je word achtervolgt door medewerkers'
+'Je komt aan bij een lege gang, een open raam en een deur waarvan je niet weet waar het naartoe gaat.'
+'Gang, Deur, Raam'
+
+'Je rent de gang uit...'
+'Je ziet de uitgang aan het eind'
+'BOEM, ineens staat de concierge voor je en pakt je bij je kraag.'
+'Je bent gepakt door de concierge.'
+'Game-Over!'
+
+'Je trekt de deur op en sluit hem direct achter.'
+'Na 3 seconden goed ademen, besef je dat je in een kast bent gestormt.'
+'Ineens trekt docent gym de deur van de kast open en trekt je de kast uit.'
+'Je bent gepakt door de gym docent en meegenomen naar het nablijf lokaal.'
+'Voor de rest van de game moet je nablijven.'
+'Game-Over!'
+
+'Je springt door het raam.'
+'Na een goeie seconden besef je je dat je je Web-Slingers niet hebt...'
+'Je viel dood op de grond'
+'Game-Over'
+
+'Je hebt de outfit "{outfit}" gekozen!'
+'Je gaat naar school maar je bent 5 minuten te laat.'
+'Je gaat naar je eerste les, wiskunde...'
+'Eenmaal binnen gekomen zie je dat de docent al boos kijkt.'
+'Je bent te laat, MAAR als je deze vraag goed beantwoord dan mag je blijven.'
+'Wat is de wortel van "4 x (7:(15-10))"?'
+'Top gedaan 5.6 is helemaal goed!'
+'Top of the class ending.'
+
+'Dat is helemaal fout, ga jij maar melden bij de directie'
+'Je bent op de gang onderweg naar de directie'
+'Alleen omdat ik maar 5 minuutjes te laat ben'
+'Ga je meldeen bij directie?'
+
+'Waarom ben je eruit gestuurd?'
+'Omdat ik 5 minuutjes te laat was...'
+'Je bent gecatched door directie, laat je nooit vangen!'
+
+'WAT JE GAAT NIET, IK WIST NIET DAT JE DIT IN JE HAD'
+'RENNEN!!!!!'
+'Je rent heel hard door de gangen van de school!'
+'Achter je merk je dat je word achtervolgt door medewerkers'
+'Je komt aan bij een lege gang, een open raam en een deur waarvan je niet weet waar het naartoe gaat.'
+'Gang, Deur, Raam'
+
+'Je rent de gang uit...'
+'Je ziet de uitgang aan het eind'
+'BOEM, ineens staat de concierge voor je en pakt je bij je kraag.'
+'Je bent gepakt door de concierge.'
+'Game-Over!'
+
+'Na 3 seconden goed ademen, besef je dat je in een kast bent gestormt.'
+'Ineens trekt docent gym de deur van de kast open en trekt je de kast uit.'
+'Je bent gepakt door de gym docent en meegenomen naar het nablijf lokaal.'
+'Voor de rest van de game moet je nablijven.'
+'Game-Over!'
+
+'Je springt door het raam.'
+'Je pakt je Web-Slingers en slingert weg'
+'Escape Ending'
+
+'Dit is geen keuze, probeer het opnieuw'
+
+'Okay Joe-'
+'JOE MAMA, HAHHAHAHAH!!!'
+'Wie ben jij dan weer?'
+'IK BEN DE RIZZLER, HAHAHAHAH!!!'
+'Ja hier heb ik totaal geen zin in.'
+'Je spel is verpest door 9 jarige tiktok kinderen.'
+'Game-Over!'
+
+'Okay {avatar}, we gaan nu beginnen.'
+'Wacht eens even...'
+'Jij ging toch dood in "EndGame"?'
+'Wacht ik google het even...'
+'JA ZIE JE WEL, JE GING ZELFS TWEE KEER DOOD!'
+'{avatar} ging al dood in de film "Avengers: Endgame".'
+'Game-Over!'
+
+'Hallo {avatar}, je bent een postbode.'
+'Laten we wat post posten!'
+'Je loopt door de wijken met HEEL VEEL post'
+'Ga je post posten?'
+
+'Top! Je hebt post gebracht!'
+'Ga je post posten?'
+
+'Je hebt niet genoeg post gebracht'
+'Je bent ontslagen!'
+'Game-Over!'
+
+'Op een manier kan je niet simpel ja of nee zeggen, het spel stopt hier voor jou.'
+'Game-Over!'
+
+'Je bent te dom om een karakter uit de lijst te kiezen, het spel stopt hier voor jou.'
+'Game-Over!'
