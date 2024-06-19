e751d976fb4c834aaa3c31e6b5254d65a5fc84dd
SummerDallison <99068199@mydavinci.nl>
2024-06-19 11:19:05 +0200

    Bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -1,20 +1,14 @@
 from termcolor import colored
 
-TEST_ANSWERS = ['a','b','c']
+TEST_ANSWERS = ['a','A','b','B','c','C']
 AVATAR_ANSWERS = ['Batman','Spider-Man','Steve','Joe','Thanos','Graig']
-CORRECT_AVATAR_ANSWER = ['Batman']
+CORRECT_AVATAR_ANSWER = 'Batman'
 
-YES_OPTIES = ['y','j','yes','ja']
-NO_OPTIES = ['n','no','nee']
+YES_OPTIONS = ['y','j','yes','ja']
+NO_OPTIONS = ['n','no','nee']
 
-INTRO_TEXTS = [
-    colored("Dag speler, dit is een adventure game waar het draait om keuzes.", 'blue'),
-    colored("Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.", 'blue'),
-    colored("A, B of C?", 'blue')
-               ]
-
-TEST_SUCCESS_TEXT = colored("Top! het gegeven antwoord is {}.", 'green')
-TEST_FAILURE_TEXT = colored("Helaas, dit was geen keuze. Probeer het opnieuw...", 'blue')
-
-AVATAR_SELECTION_TEXT = colored("Selecteer nu één van de volgende karakters: ", 'blue')
-AVATAR_PROMPT = colored("Spider-Man, Joe, Thanos, Graig", 'blue')
+INTRO_TEXTS = (
+    "Dag speler, dit is een adventure game waar het draait om keuzes.",
+    "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen."
+)
+QUESTION_TEXT = "A, B of C?"
