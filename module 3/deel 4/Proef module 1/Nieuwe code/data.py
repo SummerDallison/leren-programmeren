611a63ba3b249bceac7d1c880c50947b2309e66a
SummerDallison <99068199@mydavinci.nl>
2024-06-20 10:07:42 +0200

    bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -7,30 +7,33 @@ CORRECT_AVATAR_ANSWER = 'Batman'
 YES_OPTIONS = ['y','j','yes','ja']
 NO_OPTIONS = ['n','no','nee']
 
-#Introduction/GAME OVER texts
+#Introduction/Endings texts
 INTRO_TEXT = colored('Dag speler, dit is een adventure game waar het draait om keuzes.', 'blue')
 EXAMPLE_TEXT = colored('Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.', 'blue')
 CHOICE_PROMPT = colored('A, B of C?', 'blue')
 WRONG_CHOICE = colored('Helaas, dit was geen keuze. Probeer het opnieuw...', 'blue')
-CORRECT_CHOICE = colored('Top! het gegeven antwoord is {}.', 'green')
+CORRECT_CHOICE = colored('Top! het gegeven antwoord is {test}.', 'green')
 AVATAR_PROMPT = colored('Selecteer nu één van de volgende karakters.', 'blue')
 AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
+CLASS_ENDING = colored('Top of the class ending.', 'magenta')
+GAME_OVER = colored('Game-Over!', 'red')
 
 #Spider-Man Storyline texts
-'Je wordt om 8 uur in de ochtend wakker in je bed op halloween..'
-'Welke outfit doe je vandaag aan?'
-'Casual, Spider-Suit'
+STARTING_SCENE = colored('Je wordt om 8 uur in de ochtend wakker in je bed op halloween.', 'blue')
+OUTFIT_PROMPT = colored('Welke outfit doe je vandaag aan?', 'blue')
+OUTFIT_OPTIONS = colored('Casual, Spider-Suit', 'blue')
+CHOSEN_OUTFIT = colored('Je hebt de outfit "{outfit}" gekozen!', 'blue')
+LATE_TO_SCHOOL = colored('Je gaat naar school maar je bent 5 minuten te laat.', 'blue')
+ENTER_MATH_CLASS = colored('Je gaat naar je eerste les, wiskunde...', 'blue')
+ANGRY_TEACHER_ENCOUNTER = colored('Eenmaal binnen gekomen zie je dat de docent al boos kijkt.', 'blue')
+LATE_ARRIVAL_CONDITION = colored('Je bent te laat, MAAR als je deze vraag goed beantwoord dan mag je blijven.', 'yellow')
+MATH_QUESTION = colored('Wat is de wortel van "4 x (7:(15-10))?', 'yellow')
+CORRECT_ANSWER = colored('Top gedaan 5.6 is helemaal goed!', 'green')
+WRONG_ANSWER = colored('Dat is helemaal fout, ga jij maar melden bij de directie.', 'yellow')
 
-'Je hebt de outfit "{outfit}" gekozen!'
-'Je gaat naar school maar je bent 5 minuten te laat.'
-'Je gaat naar je eerste les, wiskunde...'
-'Eenmaal binnen gekomen zie je dat de docent al boos kijkt.'
-'Je bent te laat, MAAR als je deze vraag goed beantwoord dan mag je blijven.'
-'Wat is de wortel van "4 x (7:(15-10))"?'
-'Top gedaan 5.6 is helemaal goed!'
-'Top of the class ending.'
 
-'Dat is helemaal fout, ga jij maar melden bij de directie'
+
+
 'Je bent op de gang onderweg naar de directie'
 'Alleen omdat ik maar 5 minuutjes te laat ben'
 'Ga je melden bij directie?'
@@ -50,19 +53,16 @@ AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
 'Je ziet de uitgang aan het eind'
 'BOEM, ineens staat de concierge voor je en pakt je bij je kraag.'
 'Je bent gepakt door de concierge.'
-'Game-Over!'
 
 'Je trekt de deur op en sluit hem direct achter.'
 'Na 3 seconden goed ademen, besef je dat je in een kast bent gestormt.'
 'Ineens trekt docent gym de deur van de kast open en trekt je de kast uit.'
 'Je bent gepakt door de gym docent en meegenomen naar het nablijf lokaal.'
 'Voor de rest van de game moet je nablijven.'
-'Game-Over!'
 
 'Je springt door het raam.'
 'Na een goeie seconden besef je je dat je je Web-Slingers niet hebt...'
 'Je viel dood op de grond'
-'Game-Over'
 
 'Je hebt de outfit "{outfit}" gekozen!'
 'Je gaat naar school maar je bent 5 minuten te laat.'
@@ -93,13 +93,11 @@ AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
 'Je ziet de uitgang aan het eind'
 'BOEM, ineens staat de concierge voor je en pakt je bij je kraag.'
 'Je bent gepakt door de concierge.'
-'Game-Over!'
 
 'Na 3 seconden goed ademen, besef je dat je in een kast bent gestormt.'
 'Ineens trekt docent gym de deur van de kast open en trekt je de kast uit.'
 'Je bent gepakt door de gym docent en meegenomen naar het nablijf lokaal.'
 'Voor de rest van de game moet je nablijven.'
-'Game-Over!'
 
 'Je springt door het raam.'
 'Je pakt je Web-Slingers en slingert weg'
@@ -113,7 +111,6 @@ AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
 'IK BEN DE RIZZLER, HAHAHAHAH!!!'
 'Ja hier heb ik totaal geen zin in.'
 'Je spel is verpest door 9 jarige tiktok kinderen.'
-'Game-Over!'
 
 'Okay {avatar}, we gaan nu beginnen.'
 'Wacht eens even...'
@@ -121,7 +118,6 @@ AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
 'Wacht ik google het even...'
 'JA ZIE JE WEL, JE GING ZELFS TWEE KEER DOOD!'
 '{avatar} ging al dood in de film "Avengers: Endgame".'
-'Game-Over!'
 
 'Hallo {avatar}, je bent een postbode.'
 'Laten we wat post posten!'
@@ -133,10 +129,7 @@ AVATAR_OPTIONS = colored('Spider-Man, Joe, Thanos, Graig', 'blue')
 
 'Je hebt niet genoeg post gebracht'
 'Je bent ontslagen!'
-'Game-Over!'
 
 'Op een manier kan je niet simpel ja of nee zeggen, het spel stopt hier voor jou.'
-'Game-Over!'
 
 'Je bent te dom om een karakter uit de lijst te kiezen, het spel stopt hier voor jou.'
-'Game-Over!'
