a15a9d4c2ef47672bb1653bdcca4555e676414a1
SummerDallison <99068199@mydavinci.nl>
2024-06-25 11:17:33 +0200

    bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -1,11 +1,11 @@
 TEST_ANSWERS = ['a','A','b','B','c','C']
-AVATAR_ANSWERS = ['Batman','Spider-Man','Steve','Joe','Thanos','Graig']
-CORRECT_AVATAR_ANSWER = 'Batman'
-
 YES_OPTIONS = ['y','j','yes','ja']
 NO_OPTIONS = ['n','no','nee']
+AVATAR_ANSWERS = ['Spider-Man','Steve','Joe','Thanos','Graig']
 
-GAME_INTRO = [
-    "Dag speler, dit is een adventure game waar het draait om keuzes.",
-    "Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen."
-]
+INTRO_TEXT = 'Dag speler, dit is een adventure game waar het draait om keuzes.'
+EXAMPLE_PROMPT = 'Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.'
+TEST_PROMPT = 'A, B of C?'
+TEST_SUCCES = 'Top! het gegeven antwoord is {test}.'
+AVATAR_PROMPT = 'Selecteer nu één van de volgende karakters.'
+INVALID_CHARACTER = 'Spider-Man, Joe, Thanos, Graig'
