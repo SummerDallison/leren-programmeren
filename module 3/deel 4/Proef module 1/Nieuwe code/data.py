013fade5c90db85abcb473e5a05a9c94418b0516
SummerDallison <99068199@mydavinci.nl>
2024-06-26 13:18:47 +0200

    bijgewerkt

--- module 3/deel 4/Proef module 1/Nieuwe code/data.py
+++ module 3/deel 4/Proef module 1/Nieuwe code/data.py
@@ -3,9 +3,3 @@ YES_OPTIONS = ['y','j','yes','ja']
 NO_OPTIONS = ['n','no','nee']
 AVATAR_ANSWERS = ['Spider-Man','Steve','Joe','Thanos','Graig']
 
-INTRO_TEXT = 'Dag speler, dit is een adventure game waar het draait om keuzes.'
-EXAMPLE_PROMPT = 'Om je bekend te maken met hoe dit spel werkt zal je nu een voorbeeld vraag krijgen.'
-TEST_PROMPT = 'A, B of C?'
-TEST_SUCCES = 'Top! het gegeven antwoord is {test}.'
-AVATAR_PROMPT = 'Selecteer nu één van de volgende karakters.'
-INVALID_CHARACTER = 'Spider-Man, Joe, Thanos, Graig'
