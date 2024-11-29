288aac4ee8591b82b5fe808f93998a2026022efe
SummerDallison <99068199@mydavinci.nl>
2024-11-29 12:23:36 +0100

    Bijgewerkt

--- module 3/Proef/Code/main.py
+++ module 3/Proef/Code/main.py
@@ -1,24 +1,2 @@
-from data import PROMPT_WELKOM, AFSLUITING
+from data import *
 from functions import *
-
-def start_programma():
-    print(PROMPT_WELKOM)
-    
-    while True:
-        aantal = vraag_bolletjes()
-
-        if check_aantal_bolletjes(aantal):  
-            keuze = vraag_keuze(aantal) 
-            if keuze:
-                pass 
-
-        antwoord = vraag_meer_bestellen()
-        
-        if antwoord == 'nee':
-            print(AFSLUITING)  
-            break
-        elif antwoord != 'ja':
-            print(ERROR_ONBEKEND)  
-            continue
-
-start_programma()
