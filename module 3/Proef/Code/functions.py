d73aea3e20f4e459d415b02c11073ba337048713
SummerDallison <99068199@mydavinci.nl>
2024-11-29 12:43:33 +0100

    Bijgewerkt

--- module 3/Proef/Code/functions.py
+++ module 3/Proef/Code/functions.py
@@ -1,2 +1,27 @@
 from data import *
 
+def vraag_aantal_bolletjes():
+    try:
+        aantal_bolletjes = int(input(PROMPT_BOLLETJES))
+        return aantal_bolletjes
+    except ValueError:
+        print(ERROR_ONBEKEND)
+        return None
+
+def vraag_hoorntje_of_bakje(aantal_bolletjes):
+    while True:
+        keuze = input(PROMPT_KEUZE).lower()
+        if keuze in KEUZE_HOORNTJE_BAKJE:
+            return keuze
+        else:
+            print(ERROR_ONBEKEND)
+
+def vraag_meer_bestellen():
+    while True:
+        meer_bestellen = input(PROMPT_MEER).lower()
+        if meer_bestellen == "ja":
+            return True
+        elif meer_bestellen == "nee":
+            return False
+        else:
+            print(ERROR_ONBEKEND)
--- module 3/Proef/Code/main.py
+++ module 3/Proef/Code/main.py
@@ -1,2 +1,29 @@
 from data import *
 from functions import *
+
+def start_programma():
+    print(PROMPT_WELKOM)
+
+    while True:
+        aantal_bolletjes = vraag_aantal_bolletjes()
+
+        if aantal_bolletjes is None:
+            continue
+
+        if 1 <= aantal_bolletjes <= 3:
+            keuze = vraag_aantal_bolletjes(aantal_bolletjes)
+            print(ANTWOORD_HOORNTJE_BAKJE.format(aantal=aantal_bolletjes, keuze=keuze))
+        elif 4 <= aantal_bolletjes <= 8:
+            print(ANTWOORD_BAKJE.format(aantal=aantal_bolletjes))
+        elif aantal_bolletjes > 8:
+            print(ERROR_BAKKEN)
+            continue
+        else:
+            print(ERROR_ONBEKEND)
+            continue
+
+        if not vraag_meer_bestellen():
+            print(AFSLUITING)
+            break
+
+start_programma()
