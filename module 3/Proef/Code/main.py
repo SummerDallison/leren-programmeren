18b53d6f382a83e4971e4513c30812bb6093fae6
SummerDallison <99068199@mydavinci.nl>
2024-12-02 10:59:13 +0100

    Bijgewerkt

--- module 3/Proef/Code/main.py
+++ module 3/Proef/Code/main.py
@@ -1,29 +1,16 @@
-from data import *
 from functions import *
+from data import *
 
-def start_programma():
+def main():
     print(PROMPT_WELKOM)
 
     while True:
-        aantal_bolletjes = vraag_aantal_bolletjes()
+        aantal_bolletjes = vraag_bolletjes()
 
-        if aantal_bolletjes is None:
-            continue
-
-        if 1 <= aantal_bolletjes <= 3:
-            keuze = vraag_aantal_bolletjes(aantal_bolletjes)
-            print(ANTWOORD_HOORNTJE_BAKJE.format(aantal=aantal_bolletjes, keuze=keuze))
-        elif 4 <= aantal_bolletjes <= 8:
-            print(ANTWOORD_BAKJE.format(aantal=aantal_bolletjes))
-        elif aantal_bolletjes > 8:
-            print(ERROR_BAKKEN)
-            continue
-        else:
-            print(ERROR_ONBEKEND)
-            continue
+        if aantal_bolletjes is not None:
+            vraag_keuze(aantal_bolletjes)
 
         if not vraag_meer_bestellen():
-            print(AFSLUITING)
             break
 
-start_programma()
+main()
