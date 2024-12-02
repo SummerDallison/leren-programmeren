e753335c4561e83f982caf7d5e35f2cfa6b931a8
SummerDallison <99068199@mydavinci.nl>
2024-12-02 10:49:12 +0100

    Bijgewerkt

--- module 3/Proef/Code/functions.py
+++ module 3/Proef/Code/functions.py
@@ -1,27 +1,37 @@
 from data import *
 
-def vraag_aantal_bolletjes():
-    try:
-        aantal_bolletjes = int(input(PROMPT_BOLLETJES))
-        return aantal_bolletjes
-    except ValueError:
-        print(ERROR_ONBEKEND)
-        return None
-
-def vraag_hoorntje_of_bakje(aantal_bolletjes):
+def vraag_bolletjes():
     while True:
-        keuze = input(PROMPT_KEUZE).lower()
+        try:
+            aantal_bolletjes = int(input(PROMPT_BOLLETJES))
+            if 1 <= aantal_bolletjes <= 3:
+                return aantal_bolletjes
+            elif 4 <= aantal_bolletjes <= 8:
+                print(ANTWOORD_BAKJE.format(aantal=aantal_bolletjes))
+                return None  # Sla stap 2 over, geen keuze nodig
+            elif aantal_bolletjes > 8:
+                print(ERROR_BAKKEN)
+            else:
+                print(ERROR_ONBEKEND)
+        except ValueError:
+            print(ERROR_ONBEKEND)
+
+def vraag_keuze(aantal):
+    while True:
+        keuze = input(PROMPT_KEUZE.format(aantal=aantal)).lower()
         if keuze in KEUZE_HOORNTJE_BAKJE:
-            return keuze
+            print(ANTWOORD_HOORNTJE_BAKJE.format(keuze=keuze, aantal=aantal))
+            return
         else:
             print(ERROR_ONBEKEND)
 
 def vraag_meer_bestellen():
     while True:
-        meer_bestellen = input(PROMPT_MEER).lower()
-        if meer_bestellen == "ja":
+        meer = input(PROMPT_MEER).lower()
+        if meer == "ja":
             return True
-        elif meer_bestellen == "nee":
+        elif meer == "nee":
+            print(AFSLUITING)
             return False
         else:
             print(ERROR_ONBEKEND)
