837f72e9bc9ec5dfec2b2af9df94a7a1fc0d5b66
SummerDallison <99068199@mydavinci.nl>
2024-12-09 11:36:06 +0100

    Bijgewerkt

--- module 3/Proef/Code/functions.py
+++ module 3/Proef/Code/functions.py
@@ -8,6 +8,20 @@ def vraag_aantal_bolletjes():
         except ValueError:
             print(ERROR_ONBEKEND)
 
+def vraag_smaak_bolletje(bolletje_nummer):
+    while True:
+        keuze_smaak = input(PROMPT_SMAAK.format(X=bolletje_nummer)).lower()
+        if keuze_smaak == 'a':
+            return 'aardbei'
+        elif keuze_smaak == 'c':
+            return 'chocolade'
+        elif keuze_smaak == 'm':
+            return 'munt'
+        elif keuze_smaak == 'v':
+            return 'vanille'
+        else:
+            print(ERROR_ONBEKEND)
+
 def vraag_keuze_bakje_hoorntje(aantal_bolletjes):
     while True:
         keuze = input(PROMPT_KEUZE.format(aantal=aantal_bolletjes)).lower()
@@ -17,19 +31,33 @@ def vraag_keuze_bakje_hoorntje(aantal_bolletjes):
             print(ERROR_ONBEKEND)
 
 def antwoord_bolletjes(aantal_bolletjes):
+    smaken_lijst = []  # Om de gekozen smaken op te slaan
+
     if 1 <= aantal_bolletjes <= 3:
         keuze = vraag_keuze_bakje_hoorntje(aantal_bolletjes)
         print(ANTWOORD_HOORNTJE_BAKJE.format(keuze=keuze, aantal=aantal_bolletjes))
-        return keuze 
+
+        # Vraag per bolletje naar de smaak
+        for i in range(1, aantal_bolletjes + 1):
+            smaak = vraag_smaak_bolletje(i)
+            smaken_lijst.append(smaak)
+        
+        return keuze, smaken_lijst
     elif 4 <= aantal_bolletjes <= 8:
         print(ANTWOORD_BAKJE.format(aantal=aantal_bolletjes))
-        return "bakje"
+
+        # Vraag per bolletje naar de smaak
+        for i in range(1, aantal_bolletjes + 1):
+            smaak = vraag_smaak_bolletje(i)
+            smaken_lijst.append(smaak)
+
+        return "bakje", smaken_lijst
     elif aantal_bolletjes > 8:
         print(ERROR_BAKKEN)
-        return False  
+        return False, []  
     else:
         print(ERROR_ONBEKEND)
-        return False
+        return False, []
 
 def vraag_meer_bestellen():
     while True:
@@ -42,15 +70,17 @@ def vraag_meer_bestellen():
         else:
             print(ERROR_ONBEKEND)
 
-def print_bonnetje(totaal_bolletjes, totaal_hoorntjes, totaal_bakjes):
+def print_bonnetje(totaal_bolletjes, totaal_hoorntjes, totaal_bakjes, smaken):
     print(BEGIN_BONNETJE)
 
     prijs_bolletjes = totaal_bolletjes * BOLLETJE
     prijs_hoorntjes = totaal_hoorntjes * HOORNTJE
     prijs_bakjes = totaal_bakjes * BAKJE
 
-    if totaal_bolletjes > 0:
-        print(BON_BOLLETJES.format(totaal_bolletjes=totaal_bolletjes, BOLLETJE=BOLLETJE, prijs_bolletjes=prijs_bolletjes))
+    # Print de smaken van de bolletjes
+    for smaak in smaken:
+        print(BON_BOLLETJES(smaak=smaak, BOLLETJE=BOLLETJE))
+
     if totaal_hoorntjes > 0:
         print(BON_HOORNTJES.format(totaal_hoorntjes=totaal_hoorntjes, HOORNTJE=HOORNTJE, prijs_hoorntjes=prijs_hoorntjes))
     if totaal_bakjes > 0:
--- module 3/Proef/Code/main.py
+++ module 3/Proef/Code/main.py
@@ -7,18 +7,20 @@ def main():
     totaal_bolletjes = 0
     totaal_hoorntjes = 0
     totaal_bakjes = 0
+    smaken = []
 
     while True:
         # Vraag naar het aantal bolletjes
         aantal_bolletjes = vraag_aantal_bolletjes()
 
         # Verwerk het aantal bolletjes en de keuze voor hoorntje of bakje
-        keuze = antwoord_bolletjes(aantal_bolletjes)
+        keuze, smaken_bolletjes = antwoord_bolletjes(aantal_bolletjes)
         if not keuze:
             continue  
 
         # Tel de bestellingen op
         totaal_bolletjes += aantal_bolletjes
+        smaken.extend(smaken_bolletjes)  # Voeg de smaken toe aan de lijst
 
         # Verhoog de juiste teller afhankelijk van de keuze
         if keuze == "hoorntje":
@@ -29,7 +31,7 @@ def main():
         # Vraag of ze meer willen bestellen
         if not vraag_meer_bestellen():
             # Toon het bonnetje als ze klaar zijn
-            print_bonnetje(totaal_bolletjes, totaal_hoorntjes, totaal_bakjes)
+            print_bonnetje(totaal_bolletjes, totaal_hoorntjes, totaal_bakjes, smaken)
             break
 
 main()
