0535e3eb31f88d04c69a1bf0a0f4331c043826e6
SummerDallison <99068199@mydavinci.nl>
2024-12-09 11:14:18 +0100

    bijgewerkt

--- module 3/Proef/Code/data.py
+++ module 3/Proef/Code/data.py
@@ -3,11 +3,13 @@ HOORNTJE = 1.25
 BAKJE = 0.75
 
 KEUZE_HOORNTJE_BAKJE = ['hoorntje', 'bakje']
+KEUZE_SMAAK_BOLLETJE = ['aardbei', 'chocolade', 'munt', 'vanille']
 
 PROMPT_WELKOM = "Welkom bij Papi Gelato, je mag alle smaken kiezen zolang het maar vanille ijs is."
 PROMPT_BOLLETJES = "Hoeveel bolletjes wilt u? "
 PROMPT_KEUZE = "Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje? "
 PROMPT_MEER = "Wilt u nog meer bestellen? (ja/nee) "
+PROMPT_SMAAK = "Welke smaak wilt u voor bolletje nummer {X}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?"
 
 ERROR_ONBEKEND = "Sorry, dat snap ik niet..."
 ERROR_BAKKEN = "Sorry, zulke grote bakken hebben we niet."
